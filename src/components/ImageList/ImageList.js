94aa9f24045f016618ff14b06264be89a202ca41
Diane998 <gutierrez.almiradyan@gmail.com>
2020-11-04 02:10:47 +0800

    Initial commit

---
+++ src/components/ImageList/ImageList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ImageList.css';
+import ImageCard from '../ImageCard/ImageCard';
+
+const ImageList = ({ images }) => {
+  const imagesArr = images.map(image => (
+    <ImageCard key={image.id} image={image} />
+  ));
+
+  return <div className="image-list">{imagesArr}</div>;
+};
+
+export default ImageList;
